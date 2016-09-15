7bbc3435ba82af61eccd32a9faf8b33fc2fb0138
kfrederick5 <tefsuncle@gmail.com>
2016-09-15 01:20:02 -0700

    Added views, java docs, and listeners.

--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/occars/Car.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/occars/Car.java
@@ -2,6 +2,79 @@ package edu.orangecoastcollege.cs273.kfrederick5.occars;
 
 /**
  * Created by kfrederick5 on 9/13/2016.
+ *
+ * Used to create car objects that hold the price of
+ * a car and the loan prices and terms.
  */
 public class Car {
+
+    /**
+     * Created variables to hold values for the down
+     * payment, the loan term, the price, and the tax rate
+     * for the car object.
+     */
+    private double mDownPayment;
+    private double mLoanTerm;
+    private double mPrice;
+    private static double TAX_RATE;
+
+
+    /**
+     * returns the value of the down payment amount
+     * @return double mDownPayment
+     */
+    public double getDownPayment() {
+        return mDownPayment;
+    }
+
+    /**
+     * Sets a value for the down payment given to
+     * the car object.
+     * @param downPayment double value given
+     */
+    public void setDownPayment(double downPayment) {
+        mDownPayment = downPayment;
+    }
+
+    /**
+     * returns the value of the loan term
+     * @return
+     */
+    public double getLoanTerm() {
+        return mLoanTerm;
+    }
+
+    public void setLoanTerm(double loanTerm) {
+        mLoanTerm = loanTerm;
+    }
+
+    public double getPrice() {
+        return mPrice;
+    }
+
+    public void setPrice(double price) {
+        mPrice = price;
+    }
+
+    public void calculateBorrowedAmount(){
+
+    }
+
+    public void calculateInterestAmount(){
+
+    }
+
+    public void calculateMonthlyPayment(){
+
+    }
+
+    public void calculateTaxAmount(){
+
+    }
+
+    public void calculateTotalCost(){
+
+    }
+
+
 }
--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/occars/PurchaseActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/occars/PurchaseActivity.java
@@ -1,13 +1,85 @@
 package edu.orangecoastcollege.cs273.kfrederick5.occars;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class PurchaseActivity extends AppCompatActivity {
+
+    private EditText carPriceEditText;
+    private EditText downPaymentEditText;
+    private RadioButton threeYears;
+    private RadioButton fourYears;
+    private RadioButton fiveYears;
+    private Button loanReport;
+    private RadioGroup loanTerm;
+
+    Car newCar = new Car();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchase);
+
+        carPriceEditText = (EditText) findViewById(R.id.carPriceEditText);
+        downPaymentEditText = (EditText) findViewById(R.id.downPaymentEditText);
+        threeYears = (RadioButton) findViewById(R.id.threeYearsRadioButton);
+        fourYears = (RadioButton) findViewById(R.id.fourYearsRadioButton);
+        fiveYears = (RadioButton) findViewById(R.id.fiveYearsRadioButton);
+        loanReport =  (Button) findViewById(R.id.loanReportButton);
+        loanTerm = (RadioGroup) findViewById(R.id.loanTermRadioGroup);
+
+        carPriceEditText.addTextChangedListener(priceTextChangedListener);
+
+        downPaymentEditText.addTextChangedListener(downPaymentTextChangedListener);
+
+        loanTerm.setOnCheckedChangeListener(loanTermChangedListener);
+
     }
+
+    private TextWatcher priceTextChangedListener = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+    private TextWatcher downPaymentTextChangedListener = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+    private RadioGroup.OnCheckedChangeListener loanTermChangedListener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+        }
+    };
 }
